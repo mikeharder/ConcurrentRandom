f521680078577b8b1b8ebe368682bbe9e35c219b
Mike Harder <mharder@microsoft.com>
2017-10-11 09:45:43 -0700

    Initial project

---
+++ ConcurrentRandom/ConcurrentRandom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace ConcurrentRandom
+{
+    public class ConcurrentRandom
+    {
+        private readonly Random _seedGenerator = new Random();
+        private readonly ThreadLocal<Random> _threadLocal;
+
+        public ConcurrentRandom()
+        {
+            _threadLocal = new ThreadLocal<Random>(() =>
+            {
+                lock (_seedGenerator)
+                {
+                    return new Random(_seedGenerator.Next());
+                }
+            });
+        }
+
+        public int Next()
+        {
+            return _threadLocal.Value.Next();
+        }
+
+        public int Next(int maxValue)
+        {
+            return _threadLocal.Value.Next(maxValue);
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            return _threadLocal.Value.Next(minValue, maxValue);
+        }
+
+        public void NextBytes(byte[] buffer)
+        {
+            _threadLocal.Value.NextBytes(buffer);
+        }
+    }
+}
